4f2adcd86b80163e11eef08ff216b66b60ed0127
Michael A <mcashton06@gmail.com>
2026-02-13 20:58:58 -0800

    Flappy Bird Stage 1

--- Assets/ASSIGNMENTS/Flappy Bird/Obstacle.cs
+++ Assets/ASSIGNMENTS/Flappy Bird/Obstacle.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 public class Obstacle : MonoBehaviour
 {
     List<GameObject> obstacles = new List<GameObject>();
-    
+
+    public int score = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,8 +17,15 @@ public class Obstacle : MonoBehaviour
     {
         foreach (GameObject pipe in obstacles)
         {
-            pipe.transform.Translate(Vector3.right * Time.deltaTime * 3);
+            pipe.transform.Translate(Vector2.right * Time.deltaTime * 3);
+            if (pipe.transform.position.x > 10)
+            {
+                pipe.transform.position = new Vector2(-5, pipe.transform.position.y * Random.Range(1.0f,1.5f));
+            }
+            if (pipe.transform.position.x == 0)
+            {
+                score++;
+            }
         }
-        
     }
 }
--- Assets/ASSIGNMENTS/Flappy Bird/Player.cs
+++ Assets/ASSIGNMENTS/Flappy Bird/Player.cs
@@ -2,19 +2,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float velocity = 4f;
     private Rigidbody2D rb;
+
+    public bool isHit;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Collider2D collider = this.gameObject.GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        isHit = true;
+        print("hit");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * velocity, ForceMode2D.Impulse);
+        }
+
+        if (isHit)
+        {
+            Time.timeScale = 0;
         }
     }
+    
 }
